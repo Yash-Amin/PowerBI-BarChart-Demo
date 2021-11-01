7eb833718249aee9aa276769b05cf799f32eee65
Yash Amin <yamin@linkedin.biz>
2021-11-01 19:38:27 +0530

    Custom settings

--- src/settings.ts
+++ src/settings.ts
@@ -4,6 +4,10 @@ import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
 import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
 export class VisualSettings extends DataViewObjectsParser {
+  public customSettings: CustomSettings = new CustomSettings();
+}
 
+export class CustomSettings {
+  public barColor: string = "#2867b2";
 }
 
--- src/visual.ts
+++ src/visual.ts
@@ -78,7 +78,7 @@ export class Visual implements IVisual {
 
 
         this.barContainer
-            .attr("fill", "#2867b2")
+            .attr("fill", this.settings.customSettings.barColor)
             .selectAll("rect")
             .data(datapoints.sort((a, b) => d3.descending(a.count, b.count)))
             .enter()
